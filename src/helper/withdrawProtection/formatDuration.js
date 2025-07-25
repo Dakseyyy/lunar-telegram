3457f07c3b3a98dfbe614dec82058c786f6bf7ee
Dakseyyy <Dakseyxd@proton.me>
2025-07-25 09:26:47 +0300

    Update formatDuration.js

--- src/helper/withdrawProtection/formatDuration.js
+++ src/helper/withdrawProtection/formatDuration.js
@@ -1,11 +1,8 @@
 function formatDuration(ms) {
-  const seconds = Math.floor(ms / 1000) % 60;
-  const minutes = Math.floor(ms / (1000 * 60)) % 60;
-  const hours = Math.floor(ms / (1000 * 60 * 60)) % 24;
-  const days = Math.floor(ms / (1000 * 60 * 60 * 24));
+  const totalHours = ms / (1000 * 60 * 60); // get total hours as float
 
 
-  return Math.floor(hours);
+  return Math.ceil(totalHours)
 }
 
 module.exports = formatDuration;
